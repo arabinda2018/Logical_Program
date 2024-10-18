3ab10022859fc84d1b6bd76d8aa3e4e0810444ec
Arabinda Mohanty <arabindamohanty41@gmail.com>
2024-10-18 14:00:20 +0530

    remove junk

--- src/naveenautomation/RemoveJunk.java
+++ src/naveenautomation/RemoveJunk.java
@@ -2,6 +2,8 @@ package naveenautomation;
 
 public class RemoveJunk {
 public static void main(String[] args) {
-	
+	String s ="Core java@3424&*()$$#";
+	s=s.replaceAll("[^A-Za-z0-9]", "");
+	System.out.println(s);
 }
 }
