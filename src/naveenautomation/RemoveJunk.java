d9b711886d5e9919cdd4a3a437932e268d1379e3
Arabinda Mohanty <arabindamohanty41@gmail.com>
2024-10-18 14:12:24 +0530

    updated

--- src/naveenautomation/RemoveJunk.java
+++ src/naveenautomation/RemoveJunk.java
@@ -1,6 +1,8 @@
 package naveenautomation;
 
 public class RemoveJunk {
+	
+	//Remove junk character from the string./unwanted character from the string.
 public static void main(String[] args) {
 	String s ="Core java@3424&*()$$#";
 	s=s.replaceAll("[^A-Za-z0-9]", "");
